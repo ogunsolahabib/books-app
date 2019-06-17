50a607cdfb8577b432ca951e2349c3b0d909a7ad
ogunsolahabib <ogunsolahabib@yahoo.com>
2019-06-17 10:03:48 +0100

    Updates

---
+++ precache-manifest.5edf916ad3b51b98d1e09ac9010234a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1cd19cd88ca06b179b895fea210e57c",
+    "url": "/books-app/index.html"
+  },
+  {
+    "revision": "8e36d69b061fbb8a0dda",
+    "url": "/books-app/static/css/main.6410ea78.chunk.css"
+  },
+  {
+    "revision": "e0e79ee2a375f77a85b2",
+    "url": "/books-app/static/js/2.04e80333.chunk.js"
+  },
+  {
+    "revision": "8e36d69b061fbb8a0dda",
+    "url": "/books-app/static/js/main.d857b478.chunk.js"
+  },
+  {
+    "revision": "bb01db2d8de36c95f5da",
+    "url": "/books-app/static/js/runtime~main.c654da95.js"
+  }
+]);
